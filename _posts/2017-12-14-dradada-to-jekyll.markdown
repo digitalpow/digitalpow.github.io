7d2ec20c95d2d162dddaba9c933ee1e2f5f4ba88
Nikolozoid <nick.paatashvili@gmail.com>
2018-02-28 00:19:45 +0400

    :)
    back

--- _posts/2017-12-14-dradada-to-jekyll.markdown
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title:  "Illustration for DataFest Tbilisi"
-date:   2017-12-13 13:11:17 +0400
-categories: jekyll update
----
-![My helpful screenshot]({{ "/assets/images/chokha-minimal.png" | absolute_url }})
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 console.log(0);
+=======
+var parallax = new Parallax(document.getElementById('scene'))
+var parallax2 = new Parallax(document.getElementById('sceneDonut'))
+>>>>>>> parent of 28a6052... stage commit, nothing is finished yet
